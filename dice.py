728e8f22fd53e25713b627cb1de3e89a1907a3fa
shanidighorker <107717766+shanidighorker@users.noreply.github.com>
2024-09-14 13:25:41 +0300

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,40 @@
+""" student:shani dihorker
+ID:209290121
+Assigment no.3
+program: dice"""
+import random
+
+def dice():
+    throwonedice = int(input("please enter a number 1 \n 2 \n 3\n quit:")) # שלב בחירת ההטלה ע"י הקלדה של המשתמש אחד מהמספרים ויציאה
+    if throwonedice == 1: #שלב חישוב ההטלה הראשונה במידה והמשתמש הקיש 1
+        throwonedicet = random.randint(1, 6) #במידה והמשתמש הקיש 1 זה מטיל את הקוביה הראשונה בין 1 ל6
+        print("Result of the one dice throw:", throwonedicet)
+
+    throwonedice = int(input("please enter a number 1 \n 2 \n 3\n quit:"))
+    choice = random.randint(1,6)
+    sumchoice = throwonedice + choice
+    print(sumchoice)
+
+    if sumchoice == 12 or sumchoice <= 4: #התוכנית בודקת אם התוצאה בין 4 ל-12. אם כן, היא מדפיסה "You win!"
+        print("you win:")
+    else:
+        print("You failed, try again")
+
+    throwtwodice = int(input("please enter a number 1: \n number2: \n number3:\n quit")) # קוביה שנייה הטלה שנייה
+    if throwtwodice == 2: #שלב חישוב ההטלה השנייה(2 קוביות) במידה והמשתמש הקיש 2
+        choice1 = random.randint(1, 6) #במידה והמשתמש הקיש 2 זה מטיל את הקוביה בפעם השניה כלומר הטלה נוספת
+        choice2 = random.randint(1, 6) #במידה והמשתמש הקיש 2 זה מטיל את הקוביה בפעם השניה כלומר הטלה נוספת זאת ההטלה השניה כי זה 2 קוביות
+        choice = choice1 + choice2 #שמירת התוצאות של הטלת שתי קוביות במשתנה כדי לקבל תוצאה סופית
+        print("two dice", choice)
+
+    quit = random.randint (1,6) + random.randint (1,6)
+    sumchoice = throwonedice + quit
+    print(sumchoice)
+
+    if sumchoice == 12 or sumchoice <= 4:   # התוכנית בודקת אם התוצאה בין 4 ל-12. אם כן, היא מדפיסה "You win!".
+        print("you win!")
+    else:
+        print("You failed, try again")
+
+
+dice()
---
+++ digit.py
@@ -0,0 +1,20 @@
+""" student:shani dihorker
+ID:209290121
+Assigment no.2
+program: digit"""
+
+"""
+A program that receives a 3-digit integer checks whether the number in the input
+is really 3 digits and prints them in reverse order and converts the input to an integer.
+"""
+def digit():
+    numdigit = int(input("please enter a 3 digit number:")) #קליטה של 3 ספרות מהמשתמש
+    if numdigit < 1000 or numdigit > 99: #בדיקה עבור האם הספר שנקלט מהמשתמש מכיל בדיוק 3 ספרות
+        digit1 = (numdigit // 10) // 10  # עבור חישוב והבאת ספרת המאות
+        digit2 = (numdigit // 10) % 10  # עבור חישוב והבאת ספרת העשרות
+        digit3 = (numdigit % 10)  # עבור חישוב והבאת ספרת האחדות
+        print(digit3, "\t", digit2, "\t", digit1, "\t" ) #הדפסת הספרות שנקלטו מהמשתמש בסדר הפוך והדפסת רווחים ע"י"\t"
+
+def main():
+    digit()
+main()
